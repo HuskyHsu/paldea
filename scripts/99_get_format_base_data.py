2309e949364f115ffe03c70beba68b0c211512a0
HuskyHsu <schi.hsu@gmail.com>
2022-11-27 19:35:03 +0800

    feat: 🎸 base page

---
+++ scripts/99_get_format_base_data.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+
+def get_list():
+    response = requests.get("https://paldea.fly.dev/api/pokemons?sort=paldeaId")
+    return response.json()
+
+
+if __name__ == '__main__':
+
+    base_list = get_list()
+    
+    output = []
+    for base in base_list['data']:
+        attributes = base['attributes']
+        data = {
+            "paldeaId": attributes["paldeaId"],
+            "nationalId": attributes["nationalId"],
+            "nameZh": attributes["nameZh"],
+            "nameJp": attributes["nameJp"],
+            "nameEn": attributes["nameEn"],
+            "types": attributes["types"],
+            "stats": [
+                attributes["statsHp"],
+                attributes["statsAtk"],
+                attributes["statsDef"],
+                attributes["statsSpAtk"],
+                attributes["statsSpDef"],
+                attributes["statsSpd"],
+                attributes["statsTotal"],
+            ]
+        }
+
+        output.append(data)
+
+    with open('../src/data/base_list.json', 'wt', encoding='utf-8') as fout:
+        fout.write(json.dumps(output))
