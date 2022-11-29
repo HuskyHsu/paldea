e9182300ff50fe515506df21979d2c927bac735c
huskyHsu <schi.hsu@gmail.com>
2022-11-29 15:25:02 +0800

    feat: 🎸 add menu bar

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -3,7 +3,7 @@ import data from "../../data/base_list.json";
 
 function List() {
   return (
-    <div className="flex flex-wrap md:gap-4 gap-2 bg-orange-50 px-4 justify-around">
+    <div className="flex flex-wrap md:gap-4 gap-2 px-4 justify-around rounded-xl bg-orange-50">
       {data.map((pm) => (
         <Card pokemon={pm} key={pm.paldeaId.toString() + pm.altForm} />
       ))}
