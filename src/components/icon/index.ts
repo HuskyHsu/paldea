cad27d230dbddbe098089b77c8e1beda6708e1b6
HuskyHsu <schi.hsu@gmail.com>
2022-11-30 21:14:53 +0800

    feat: 🎸 svg component to file

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,14 +1,15 @@
-import { Bag } from './Bag';
-import { Book } from './Book';
-import { Books } from './Books';
-import { Compass } from './Compass';
-import { Type } from './TypeIcon';
+import { ReactComponent as Bag } from '../../assets/icon/Bag.svg';
+import { ReactComponent as Book } from '../../assets/icon/Book.svg';
+import { ReactComponent as Books } from '../../assets/icon/Books.svg';
+import { ReactComponent as Compass } from '../../assets/icon/Compass.svg';
 import { ReactComponent as Scarlet } from '../../assets/icon/Scarlet.svg';
 import { ReactComponent as Violet } from '../../assets/icon/Violet.svg';
+
+import { Type } from './TypeIcon';
 
 const Version = {
   Scarlet,
   Violet,
 };
 
-export { Bag, Book, Books, Compass, Type, Version };
+export { Bag, Book, Books, Compass, Version, Type };
--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -5,35 +5,35 @@ import { Icon } from '../../components';
 
 function Main() {
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className='flex flex-col md:flex-row'>
       <aside
         className={clsx(
-          'flex flex-col w-screen md:w-72 md:h-screen md:p-4 space-y-2',
+          'flex flex-col w-screen md:w-64 md:h-screen md:p-4 space-y-2',
           'border-gray-200'
         )}
       >
         <div
           className={clsx(
-            'flex md:flex-col md:p-4 md:space-y-4 justify-around',
+            'flex md:flex-col md:p-4 md:space-y-2 justify-around',
             'md:rounded-xl',
-            'bg-primary text-white'
+            'bg-primary text-white text-sm'
           )}
         >
-          <Menu.Item text={'圖鑑清單'} color="bg-custom-red" selected={true}>
-            <Icon.Books />
+          <Menu.Item text={'圖鑑清單'} color='bg-custom-red' selected={true}>
+            <Icon.Books className='w-5 h-5 fill-current' />
           </Menu.Item>
-          <Menu.Item text={'地圖'} color="bg-custom-blue">
-            <Icon.Compass />
+          <Menu.Item text={'地圖'} color='bg-custom-blue'>
+            <Icon.Compass className='w-5 h-5 fill-current' />
           </Menu.Item>
-          <Menu.Item text={'招式清單'} color="bg-custom-green">
-            <Icon.Book />
+          <Menu.Item text={'招式清單'} color='bg-custom-green'>
+            <Icon.Book className='w-5 h-5 fill-current' />
           </Menu.Item>
-          <Menu.Item text={'道具清單'} color="bg-custom-orange">
-            <Icon.Bag />
+          <Menu.Item text={'道具清單'} color='bg-custom-orange'>
+            <Icon.Bag className='w-5 h-5 fill-current' />
           </Menu.Item>
         </div>
       </aside>
-      <div className="w-full md:p-4 max-h-screen overflow-y-auto">
+      <div className='w-full md:p-4 max-h-screen overflow-y-auto'>
         <Outlet />
       </div>
     </div>
