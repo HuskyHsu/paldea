7ff5a7f6753f04956936de2ad664f12523f1cecd
HuskyHsu <schi.hsu@gmail.com>
2022-11-30 21:46:51 +0800

    feat: 🎸 add bg

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -9,12 +9,12 @@ function List() {
   const { types, targetType } = useFilterStore((state) => state);
 
   return (
-    <div className="rounded-xl bg-orange-50">
-      <form className="pt-6 px-4">
-        <ul className="flex flex-col items-center gap-y-8">
-          <li className="w-full md:w-5/6 flex flex-wrap justify-center items-center gap-4">
+    <div className='rounded-xl bg-orange-50'>
+      <form className='pt-6 px-4'>
+        <ul className='flex flex-col items-center gap-y-8'>
+          <li className='w-full md:w-5/6 flex flex-wrap justify-center items-center gap-4'>
             {Object.keys(TypeMap).map((type) => (
-              <div key={type} className="group relative h-8">
+              <div key={type} className='group relative h-8'>
                 <Icon.Type
                   type={type}
                   className={clsx('w-8 h-8', {
@@ -28,7 +28,7 @@ function List() {
           </li>
         </ul>
       </form>
-      <div className="flex flex-wrap justify-around gap-2 p-4">
+      <div className='flex flex-wrap justify-around gap-4 p-4'>
         {data.map((pm) => (
           <Card pokemon={pm} key={pm.paldeaId.toString() + pm.altForm} />
         ))}
