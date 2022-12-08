dacaaf5722989dc01d2f3d2c05cb760415945153
HuskyHsu <schi.hsu@gmail.com>
2022-12-08 23:13:32 +0800

    feat: 🎸 update version button

--- src/components/Card/Footer.tsx
+++ src/components/Card/Footer.tsx
@@ -2,18 +2,21 @@ import clsx from 'clsx';
 import { VersionType } from '@/models';
 import { Icon } from '..';
 import { CardProps } from './Card';
+import { useFilterActions } from '@/store';
 
 export function Footer({ pokemon }: CardProps) {
   const version = pokemon.version ? VersionType[pokemon.version as keyof typeof VersionType] : '';
+  const { updateKeyword } = useFilterActions();
 
   return (
-    <div
+    <button
       className={clsx(
         'flex w-full justify-center',
         'rounded-b-xl py-1',
         'text-sm text-white',
         pokemon.version === 'Scarlet' ? 'bg-scarlet' : 'bg-violet'
       )}
+      onClick={() => updateKeyword(version)}
     >
       <p className="flex w-full items-center justify-center md:justify-start md:pl-4">
         {pokemon.version === 'Scarlet' ? (
@@ -25,6 +28,6 @@ export function Footer({ pokemon }: CardProps) {
         限定
         {`(${version})`}
       </p>
-    </div>
+    </button>
   );
 }
