6d706b700c76f902f95596a7b3b65590d7b784d9
huskyHsu <schi.hsu@gmail.com>
2022-12-15 17:06:32 +0800

    feat: 🎸 add auto-load paging feature in List page

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,33 +1,50 @@
-import { memo } from 'react';
+import { memo, useRef } from 'react';
 import clsx from 'clsx';
 import { useNavigate } from 'react-router-dom';
 import { BasePokemon } from '@/models';
 import { Footer } from './Footer';
 import { Body } from './Body';
 import { Header } from './Header';
+import { useIntersectionObserver } from './Hooks';
 
 export interface CardProps {
   pokemon: BasePokemon;
 }
 
-export const Card = memo(({ pokemon }: CardProps) => {
-  const navigate = useNavigate();
-  function handleClick() {
-    navigate(`/${pokemon.link}`);
+export const Card = memo(
+  ({
+    pokemon,
+    maxIndex,
+    index,
+    updateDisplayCount,
+  }: CardProps & { maxIndex: number; index: number; updateDisplayCount: Function }) => {
+    const navigate = useNavigate();
+    function handleClick() {
+      navigate(`/${pokemon.link}`);
+    }
+
+    const ref = useRef<HTMLDivElement | null>(null);
+    const entry = useIntersectionObserver(ref, {});
+    const isVisible = !!entry?.isIntersecting;
+
+    if (isVisible && maxIndex === index + 1) {
+      updateDisplayCount();
+    }
+
+    return (
+      <div
+        className={clsx('-mt-2 w-[calc(100%/3-12px)] md:-mt-6 md:max-w-[180px]', {
+          hidden: !pokemon.display,
+        })}
+        ref={ref}
+      >
+        <button className="w-full" onClick={handleClick}>
+          <Header paldeaId={pokemon.paldeaId} name={pokemon.nameZh} link={pokemon.link} />
+          <Body pokemon={pokemon} />
+
+          {pokemon.version && <Footer pokemon={pokemon} />}
+        </button>
+      </div>
+    );
   }
-
-  return (
-    <div
-      className={clsx('-mt-2 w-[calc(100%/3-12px)] md:-mt-6 md:max-w-[180px]', {
-        hidden: !pokemon.display,
-      })}
-    >
-      <button className='w-full' onClick={handleClick}>
-        <Header paldeaId={pokemon.paldeaId} name={pokemon.nameZh} link={pokemon.link} />
-        <Body pokemon={pokemon} />
-
-        {pokemon.version && <Footer pokemon={pokemon} />}
-      </button>
-    </div>
-  );
-});
+);
--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Card } from '@/components';
 import { useFilterStore } from '@/store';
 import { Header } from './Header';
@@ -5,13 +7,31 @@ import { Header } from './Header';
 function List() {
   const pokemonList = useFilterStore((state) => state.pokemonList);
 
+  const [displayCount, setDisplayCount] = useState(20);
+
+  const updateDisplayCount = () => {
+    setDisplayCount((prev) => prev + 20);
+  };
+
   return (
     <div className="min-h-full rounded-xl bg-custom-lightgrey drop-shadow-xl">
       <Header />
       <div className="flex flex-wrap justify-around gap-4 p-4">
-        {pokemonList.map((pm, i) => (
-          <Card pokemon={pm} key={pm.paldeaId.toString() + pm.altForm + String(i)} />
-        ))}
+        {pokemonList
+          .filter((pm) => pm.display)
+          .filter((_, i) => i < displayCount)
+          .map((pm, i) => {
+            const key = pm.paldeaId.toString() + pm.altForm + String(i);
+            return (
+              <Card
+                pokemon={pm}
+                maxIndex={displayCount}
+                index={i}
+                updateDisplayCount={updateDisplayCount}
+                key={key}
+              />
+            );
+          })}
       </div>
     </div>
   );
