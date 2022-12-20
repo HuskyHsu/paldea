a80e61f7920f137847401e0488eaf20a75adcfd5
HuskyHsu <schi.hsu@gmail.com>
2022-12-21 00:00:57 +0800

    feat: 🎸 udpate move info

--- scripts/98_get_relation_data.py
+++ scripts/98_get_relation_data.py
@@ -16,10 +16,27 @@ def get_abilities_list():
     return response.json()
 
 
+def get_move_list(start):
+
+    params = {
+        "populate[0]": "type",
+        "populate[1]": "levelingUps.pokemon",
+        "populate[2]": "technicalMachine.pokemon",
+        "populate[3]": "eggPokemons&sort[0]=pid",
+        "pagination[limit]": 50,
+        "pagination[start]": start,
+    }
+
+    response = requests.get(
+        f"https://paldea.fly.dev/api/moves",
+        params=params,
+    )
+    return response.json()
+
+
 if __name__ == "__main__":
 
     base_list = get_abilities_list()
-
     output = []
     for base in base_list["data"]:
         attributes = base["attributes"]
@@ -35,6 +52,7 @@ if __name__ == "__main__":
             "nameZh": attributes["nameZh"],
             "nameJp": attributes["nameJp"],
             "nameEn": attributes["nameEn"],
+            "description": attributes["description"],
             "pokemonList": [
                 pm["attributes"]["link"] for pm in attributes["pokemonList"]["data"]
             ],
@@ -49,3 +67,41 @@ if __name__ == "__main__":
         f"../public/data/relation/abilities.json", "wt", encoding="utf-8"
     ) as fout:
         fout.write(json.dumps(output))
+
+    for i in range(0, 900, 50):
+        base_list = get_move_list(i)
+
+        for base in base_list["data"]:
+            attributes = base["attributes"]
+            print(attributes["nameZh"])
+
+            if len(attributes["levelingUps"]["data"]) == 0:
+                continue
+
+            data = {
+                "nameZh": attributes["nameZh"],
+                "nameJp": attributes["nameJp"],
+                "nameEn": attributes["nameEn"],
+                "type": attributes["type"]["data"]["attributes"]["nameZh"],
+                "category": attributes["category"],
+                "power": attributes["power"],
+                "accuracy": attributes["accuracy"],
+                "PP": attributes["PP"],
+                "description": attributes["description"],
+                "levelingUps": [
+                    {
+                        "level": levelingUp["attributes"]["level"],
+                        "link": levelingUp["attributes"]["pokemon"]["data"][
+                            "attributes"
+                        ]["link"],
+                    }
+                    for levelingUp in attributes["levelingUps"]["data"]
+                ],
+            }
+
+            with open(
+                f"../public/data/relation/moves/{attributes['pid']}.json",
+                "wt",
+                encoding="utf-8",
+            ) as fout:
+                fout.write(json.dumps(data))
