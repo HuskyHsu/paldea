b47718da04e47f55ccd6264e94e3599e8c65d453
HuskyHsu <schi.hsu@gmail.com>
2023-01-25 00:36:14 +0800

    feat: 🎸 update share component

--- src/pages/Detail/components/Share.tsx
+++ src/pages/Detail/components/Share.tsx
@@ -203,65 +203,83 @@ export function LineShare({ pokemon, terasType, moves }: Porps) {
   const { status, login, logout, share } = useLiffContext();
 
   return (
-    <div>
+    <div className="pt-4">
       {status.isLoggedIn && (
-        <>
-          <div className="relative z-0 mt-8 w-44">
+        <div className="flex items-end justify-between gap-4">
+          <div className="relative z-0 mt-8 w-28 md:w-40">
             <input
               type="text"
               id="floating_standard"
-              className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent py-2.5 px-0 text-sm text-gray-100 focus:border-blue-800 focus:outline-none focus:ring-0"
+              className="peer block w-full appearance-none border-0 border-b-2 border-gray-100 bg-transparent px-0 pt-2 pb-1 text-sm text-gray-100 focus:border-green-800 focus:outline-none focus:ring-0"
               placeholder=" "
               value={code ?? ''}
-              onChange={(e) => setCode(e.target.value.toUpperCase())}
+              onChange={(e) =>
+                setCode(
+                  e.target.value
+                    .replace(/[^A-Za-z0-9\s]/g, '')
+                    .toUpperCase()
+                    .replace(/([A-Za-z0-9]{3})([A-Za-z0-9]{1,3})/, '$1-$2')
+                    .slice(0, 7)
+                )
+              }
             />
             <label
               htmlFor="floating_standard"
-              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-100 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-800"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-100 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-green-800"
             >
-              太晶團體戰邀請代碼：
+              團戰密語：
             </label>
           </div>
-          <button
-            className="mr-2 mb-2 mt-2 rounded-lg bg-custom-green px-2 py-1 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300"
-            onClick={() => {
-              share([
-                {
-                  type: 'flex',
-                  altText: `${pokemon.nameZh}-太晶團戰: ${code}`,
-                  contents: JSON.parse(
-                    JSON.stringify(
-                      genFlex({
-                        code: code,
-                        pm: pokemon,
-                        terasType: terasType,
-                        moves: moves,
-                      })
-                    )
-                  ),
-                },
-              ]);
-            }}
-          >
-            Share
-          </button>
-          <button
-            type="button"
-            className="mr-2 mb-2 mt-2 rounded-lg bg-custom-green px-2 py-1 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300"
-            onClick={logout}
-          >
-            登出
-          </button>
-        </>
+          <div className="flex items-center gap-4">
+            <button
+              className="whitespace-nowrap rounded-lg bg-custom-green px-2 py-1 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-1 focus:ring-green-300"
+              onClick={() => {
+                if (terasType === null) {
+                  return alert('請選擇太晶屬性');
+                }
+
+                if (code.length !== 7) {
+                  return alert('請檢查密語長度');
+                }
+
+                share([
+                  {
+                    type: 'flex',
+                    altText: `${pokemon.nameZh}-太晶團戰: ${code}`,
+                    contents: JSON.parse(
+                      JSON.stringify(
+                        genFlex({
+                          code: code,
+                          pm: pokemon,
+                          terasType: terasType,
+                          moves: moves,
+                        })
+                      )
+                    ),
+                  },
+                ]);
+              }}
+            >
+              傳送Line訊息
+            </button>
+            <button
+              type="button"
+              className="whitespace-nowrap rounded-lg bg-custom-green px-2 py-1 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-1 focus:ring-green-300"
+              onClick={logout}
+            >
+              登出
+            </button>
+          </div>
+        </div>
       )}
 
       {!status.isLoggedIn && (
         <button
           type="button"
-          className="mr-2 mb-2 mt-8 rounded-lg bg-custom-green px-2 py-1 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300"
+          className="rounded-lg bg-custom-green px-2 py-1 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-1 focus:ring-green-300"
           onClick={login}
         >
-          登入Line快速分享
+          登入Line，分享團戰密語與資訊
         </button>
       )}
     </div>
