49d3fb8df0ab94dadd4c0b76b4824ea28372b021
HuskyHsu <schi.hsu@gmail.com>
2023-01-26 19:50:18 +0800

    feat: 🎸 update liff bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Router } from './routers';
 
 function App() {
+  if (window.location.hash.startsWith('#context_token')) {
+    window.location.href = `${window.location.origin}${window.location.pathname}`;
+  }
   return <Router />;
 }
 
