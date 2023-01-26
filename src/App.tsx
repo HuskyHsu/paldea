0936049270792fba3877a05e165f6b5dc47adb2b
HuskyHsu <schi.hsu@gmail.com>
2023-01-26 20:04:17 +0800

    feat: 🎸 fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { Router } from './routers';
 
 function App() {
-  if (window.location.hash.startsWith('#context_token')) {
-    window.location.href = `${window.location.origin}${window.location.pathname}`;
-  }
   return <Router />;
 }
 
