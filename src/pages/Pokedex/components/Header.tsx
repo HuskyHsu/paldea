cb964c8d6d6f416a80ba9a94863143675a21b535
HuskyHsu <schi.hsu@gmail.com>
2024-01-18 23:25:13 +0800

    feat: 🎸 update tags

--- src/pages/Pokedex/components/Header.tsx
+++ src/pages/Pokedex/components/Header.tsx
@@ -30,16 +30,12 @@ export function Header({
   const abilityUpdate = updateState('ability');
   const showChildUpdate = updateState('onlyEvolution');
 
-  const hasFilter = [filter.ability, filter.EV].filter(Boolean).length > 0 || filter.tags.size > 0;
+  const hasFilter = [filter.ability, filter.EV].filter(Boolean).length > 0;
 
   const filterList = [
     { value: filter.ability, name: '特性', key: 'ability' },
     { value: EVName[filter.EV as keyof typeof EVName] || '', name: '努力值', key: 'EV' },
   ];
-
-  const filterListSet = [
-    [...filter.tags].map((tag) => ({ value: tag, name: 'tag', key: 'tags' })),
-  ].flat();
 
   return (
     <header>
@@ -100,11 +96,10 @@ export function Header({
             </button>
           ))}
         </div>
-        <SubTitleSlide title="tags(複選，交集)" />
+        <SubTitleSlide title="標籤(複選，交集)" />
         <Buttons
           list={[
-            { name: '傳說的寶可夢', val: '傳說的寶可夢' },
-            { name: '幻之寶可夢', val: '幻之寶可夢' },
+            { name: '傳說與幻獸', val: '傳說與幻獸' },
             { name: '悖謬寶可夢', val: '悖謬寶可夢' },
             { name: '准傳說的寶可夢', val: '准傳說的寶可夢' },
             { name: '朱版限定', val: '朱版限定' },
@@ -193,24 +188,6 @@ export function Header({
                 />
               </span>
             ))}
-          {filterListSet.map(({ value, name, key }) => (
-            <span
-              key={`${name}-${value}`}
-              className={clsx(
-                'flex items-center gap-x-2',
-                'rounded-xl bg-amber-100 px-2 py-1 shadow-list-items',
-                'whitespace-nowrap'
-              )}
-            >
-              {name}：{value}
-              <Icon.Close
-                className="h-3 w-3 cursor-pointer"
-                onClick={() => {
-                  updateSetState(key as keyof typeof updateSetState)(value);
-                }}
-              />
-            </span>
-          ))}
         </div>
       </div>
     </header>
